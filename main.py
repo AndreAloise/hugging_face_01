0969d502af627cda85efd2205f98bbfe5c4622e0
AndreAloise <andre.aloise.77@gmail.com>
2024-09-06 18:50:09 -0300

    Updating main to have categories examples

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pandas import DataFrame
 
+from ia_models.category_classifier import CategoryClassifier
+from ia_models.category_classifier_br import CategoryClassifierBr
 from ia_models.sentiment_classifier import SentimentClassifier
 from ia_models.sentiment_classifier_br import SentimentClassifierBr
+from services.category_classifier_br_service import CategoryClassifierBrService
 from services.cloud_words_service import CloudWordsService
 from services.sentiment_classifier_br_service import SentimentClassifierBrService
 
@@ -47,9 +50,54 @@ def get_cloud_words(converted_data: DataFrame):
 	cloud_words_service.create_cloud_words(converted_data, 'Resenha', 'POS')
 
 
+def _get_category_prediction():
+	text = 'Latest model of smartphone with 5G connectivity and 128GB internal storage'
+	categories = ['electronics', 'food', 'toys', 'books']
+	category_classifier = CategoryClassifier()
+	predictions = category_classifier.execute_prediction(text, categories)
+
+	print(predictions)
+
+
+def get_category_classifier_br():
+	# description = 'Este water gel leve e refrescante, proporciona hidratação imediata que ajuda a aliviar o repuxamento e aspereza da pele sensível.'
+	description = '''A fritadeira eletrica sem óleo start fry da elgin possui um design único, capacidade para até 3,5 litros,
+            potência de 1400w e revestimento antiaderente. Seu sistema de circulação de ar ultra rápido frita e economiza energia.
+            Sua grelha de fritura é removível e super fácil de limpar. Ela conta com uma proteção contra super aquecimento.
+            Possui controle de temperatura de 80°c a 200°c que permite você programar a temperatura de preparo para cada tipo de alimento,
+            timer para até 60 minutos com aviso sonoro e desligamento automático, assim você pode deixar preparando sua refeição
+            enquanto realiza outras tarefas.'''
+	categories = ['beleza', 'livros', 'cozinha']
+	category_classifier_br = CategoryClassifierBr(categories)
+	classification = category_classifier_br.get_classifier_of_description(description, categories)
+
+	classification_without_sequence = CategoryClassifierBrService.remove_description_from_classification(classification)
+	print(classification_without_sequence)
+
+
+def _get_product_description_from_csv():
+	service = CategoryClassifierBrService()
+	raw_data = service.get_dados()
+	CategoryClassifierBrService.save_data_to_txt(raw_data)
+
+
+def add_category_to_data():
+	categories = ['eletrodomésticos', 'eletrônicos', 'beleza', 'brinquedos']
+	category_classifier_br = CategoryClassifierBr(categories)
+
+	service = CategoryClassifierBrService()
+	service.add_category_column_to_data(category_classifier_br.get_max_score_categories)
+	updated_data = service.get_dados()
+	CategoryClassifierBrService.save_data_to_txt(updated_data)
+
+
 if __name__ == '__main__':
-	sentiment_data = _get_sentiment_prediction_from_csv()
+	# sentiment_data = _get_sentiment_prediction_from_csv()
 	# execute_ia_classifier()
 	# save_data_to_txt_file()
 	# get_sentiment_graph()
-	get_cloud_words(sentiment_data)
+	# get_cloud_words(sentiment_data)
+	# _get_category_prediction()
+	# get_category_classifier_br()
+	# _get_product_description_from_csv()
+	add_category_to_data()
