8eb8852076d55ef164d6ce1ae81408f5aa4676ba
AndreAloise <andre.aloise.77@gmail.com>
2024-09-06 18:46:20 -0300

    Updating with category list to initializer and created a function to get the max score category.

--- ia_models/category_classifier_br.py
+++ ia_models/category_classifier_br.py
@@ -4,16 +4,26 @@ from transformers import T5Tokenizer
 
 
 class CategoryClassifierBr:
-	def __init__(self):
+	def __init__(self, categories):
+		self.categories = categories
 		self.model = T5ForZeroShotClassification.from_pretrained('knowledgator/comprehend_it-multilingual-t5-base')
 		self.tokenizer = T5Tokenizer.from_pretrained('knowledgator/comprehend_it-multilingual-t5-base')
 		self.classifier = (
 			ZeroShotClassificationPipeline(model=self.model, tokenizer=self.tokenizer,
 			                               hypothesis_template='{}', encoder_decoder=True))
 
-	def get_classifier_of_description(self, description: str, categories: []):
+	def get_classifier_of_description(self, description: str):
 		"""
 			Defines just one category classification for the product description
 		"""
-		classification = self.classifier(description, categories, multi_label=False)
+		classification = self.classifier(description, self.categories, multi_label=False)
 		return classification
+
+	def get_max_score_categories(self, description):
+		"""
+			Get the max score categories from a category classification.
+		"""
+		classification = self.get_classifier_of_description(description)
+		max_categories = (
+			max(zip(classification['labels'], classification['scores']), key=lambda x: x[1]))[0]
+		return max_categories
