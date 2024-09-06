4cd5682463ae2a3a79ccbbb172be7cfe437996dc
AndreAloise <andre.aloise.77@gmail.com>
2024-09-06 18:47:37 -0300

    Created functions to save data to a 'txt' file and to add new column 'Categoria' to the csv data.

--- services/category_classifier_br_service.py
+++ services/category_classifier_br_service.py
@@ -1,11 +1,28 @@
 import pandas as pd
+from pandas import DataFrame
+
+pd.set_option('display.max_colwidth', None)
 
 
 class CategoryClassifierBrService:
 	def __init__(self):
-		pass
+		self.dados = pd.read_csv('data_files/inputs/descricoes_produtos.csv')
 
 	@staticmethod
 	def remove_description_from_classification(classification):
 		result = pd.DataFrame(classification).drop(['sequence'], axis=1)
 		return result
+
+	def get_dados(self):
+		return self.dados
+
+	@staticmethod
+	def save_data_to_txt(data: DataFrame, file_name='data_files/outputs/descricoes_produtos_output.txt'):
+		with open(file_name, 'w', encoding='utf-8') as f:
+			f.write(data.to_string(index=False))
+
+	def add_category_column_to_data(self, max_categories_func):
+		"""
+			Add new column 'Categoria' to the csv data, by a function
+		"""
+		self.dados['Categoria'] = self.dados['Descrição'].apply(max_categories_func)
