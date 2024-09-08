2dfddb85835cc8e76d130527b70c7ba38658bb27
AndreAloise <andre.aloise.77@gmail.com>
2024-09-08 19:56:54 -0300

    Updating main file with faq view example

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from pandas import DataFrame
 from ia_models.background_removal import BackgroundRemoval
 from ia_models.category_classifier import CategoryClassifier
 from ia_models.category_classifier_br import CategoryClassifierBr
+from ia_models.faq_question_answer import FaqQuestionAnswer
 from ia_models.sentiment_classifier import SentimentClassifier
 from ia_models.sentiment_classifier_br import SentimentClassifierBr
 from interface_views.background_removal_view import BackgroundRemovalView
+from interface_views.faq_question_answer_view import FaqQuestionAnswerView
 from services.category_classifier_br_service import CategoryClassifierBrService
 from services.cloud_words_service import CloudWordsService
 from services.sentiment_classifier_br_service import SentimentClassifierBrService
@@ -104,6 +106,23 @@ def create_image_view():
 	view.create_gradio_interface()
 
 
+def create_faq_view():
+	contexts = {
+		"How do I create an account?": "You can create an account by clicking on the 'Sign Up' button on our homepage. You will need to provide your email address, create a password, and fill in some basic information about yourself. An account will help you track your orders, manage your personal settings, and speed up future transactions.",
+		"Which payment methods do you accept?": "We accept a wide range of payment methods including Visa, MasterCard, American Express, Discover, PayPal, and Apple Pay. You can also pay using store credit or gift cards issued by our company.",
+		"How can I track my order?": "Once your order has shipped, you will receive an email with a tracking number. You can use this number on our website's tracking page to see the current status of your delivery.",
+		"Do you offer international shipping?": "Yes, we offer international shipping to most countries. Shipping costs and delivery times vary depending on the destination. All applicable customs fees, taxes, and duties are the responsibility of the customer and are calculated at checkout.",
+		"How long does delivery take?": "For standard shipping, deliveries typically take between 3 to 5 business days. For expedited shipping, expect your order to arrive within 1 to 2 business days. Delivery times may vary based on your location and the time of the year.",
+		"What is your return policy?": "Our return policy allows you to return products within 30 days of receiving them. Items must be in their original condition and packaging. Some items, such as perishable goods, are not eligible for return.",
+		"Can I change or cancel my order after it's been placed?": "You can change or cancel your order within 24 hours of placing it without any additional charge.To make changes or cancel your order, please contact our customer service immediately.",
+		"What should I do if I receive a damaged item?": "If you receive a damaged item, please contact our customer service within 48 hours of delivery to report the damage. You will need to provide your order number, the description of the damage, and photographic evidence. We will arrange for a replacement or refund as appropriate.",
+		"How do I reset my password?": "If you've forgotten your password, go to the login page and click on 'Forgot Password'. Enter your email address and we will send you a link to reset your password. For security purposes, this link will expire within 24 hours."
+	}
+	model = FaqQuestionAnswer(contexts)
+	view = FaqQuestionAnswerView(model)
+	view.create_gradio_interface()
+
+
 if __name__ == '__main__':
 	# sentiment_data = _get_sentiment_prediction_from_csv()
 	# execute_ia_classifier()
@@ -115,4 +134,5 @@ if __name__ == '__main__':
 	# _get_product_description_from_csv()
 	# add_category_to_data()
 	# get_cleaned_image()
-	create_image_view()
+	# create_image_view()
+	create_faq_view()
